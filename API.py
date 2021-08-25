81d8b1106cbae819460e28ec94b95c3e5bd77e01
denhur62 <denhur62@naver.com>
2021-08-25 22:48:34 +0900

    API and app modified

---
+++ API.py
@@ -0,0 +1,32 @@
+import Fasttext
+import LDA
+
+
+def recommand(user_id):
+    FT_recommand = Fasttext.recommand(user_id)
+    LDA_recommand = LDA.recommand(user_id)
+    result = []
+    for i in range(len(FT_recommand)):
+        temp = {}
+        temp['id'] = FT_recommand[i]['id']
+        temp['score'] = FT_recommand[i]['similar']+LDA_recommand[i]['similar']
+        result.append(temp)
+    result.sort(key=lambda x: x['score'], reverse=True)
+    temp = []
+    for i in result:
+        temp.append(i['id'])
+    return temp
+
+
+def vector(diary_id, title, content):
+    Fasttext.insert_diary_vec(diary_id, title, content)
+    LDA.insert_diary_vec(diary_id, title, content)
+    LDA.insert_topic(diary_id, title, content)
+
+
+def click_emotion(user_id, diary_id):
+    LDA.emotion_click(user_id, diary_id)
+
+
+def click_diary(user_id, diary_id):
+    LDA.diary_click(user_id, diary_id)
--- app.py
+++ app.py
@@ -1,12 +1,52 @@
-from flask import Flask, render_template
-
-# Flask 객체 인스턴스 생성
+from flask import Flask, jsonify, abort, request
+import API
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 
 @app.route('/')  # 접속하는 url
 def index():
-    return render_template('index.html')
+    return 'hello world'
+
+# 감정 클릭 시 토픽 추가
+
+
+@app.route('/click-emotion/<userId>/<diaryId>')
+def click_emotion(userId=None, diaryId=None):
+    API.click_emotion(userId, diaryId)
+    return '', 202
+
+# 다이어리 클릭 시 토픽 추가
+
+
+@app.route('/click-diary/<userId>/<diaryId>')
+def click_diary(userId=None, diaryId=None):
+    API.click_diary(userId, diaryId)
+    return '', 202
+# recommand
+
+
+@app.route('/recommand/<userId>')
+def recommand(userId):
+    result = API.recommand(userId)
+    return jsonify({'diaries': result})
+
+# 다이어리 생성시,수정 시  두 개 벡터 추가 및 토픽 추가 테스트
+
+
+@app.route('/diary/create', methods=['POST'])
+def vector():
+    params = request.get_json()
+    API.vector(params['diary_id'], params['title'], params['content'])
+    return '', 202
+
+# white list
+
+
+# @app.before_request
+# def limit_remote_addr():
+#     if request.remote_addr != '127.0.1.1':
+#         abort(403)  # Forbidden
 
 
 if __name__ == "__main__":
