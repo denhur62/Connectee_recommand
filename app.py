7db93ef023cc756df3592d3450746e112c616c38
denhur62 <denhur62@naver.com>
2021-08-29 01:23:52 +0900

    add app.py

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask, jsonify, abort, request
 import API
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
+from train import FTtrain, LDAtrain
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
-@app.route('/')  # 접속하는 url
+def backgroundScheduler():
+    FTtrain()
+    LDAtrain()
+
+
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(backgroundScheduler, 'interval', minutes=180)
+scheduler.start()
+application = Flask(__name__)
+application.config['JSON_AS_ASCII'] = False
+
+
+@application.route('/')  # 접속하는 url
 def index():
     return 'hello world'
 
 # 감정 클릭 시 토픽 추가
 
 
-@app.route('/click-emotion/<userId>/<diaryId>')
+@application.route('/click-emotion/<userId>/<diaryId>')
 def click_emotion(userId=None, diaryId=None):
     API.click_emotion(userId, diaryId)
     return '', 202
@@ -19,22 +31,22 @@ def click_emotion(userId=None, diaryId=None):
 # 다이어리 클릭 시 토픽 추가
 
 
-@app.route('/click-diary/<userId>/<diaryId>')
+@application.route('/click-diary/<userId>/<diaryId>')
 def click_diary(userId=None, diaryId=None):
     API.click_diary(userId, diaryId)
     return '', 202
 # recommand
 
 
-@app.route('/recommand/<userId>')
+@application.route('/recommand/<userId>')
 def recommand(userId):
     result = API.recommand(userId)
     return jsonify({'diaries': result})
 
-# 다이어리 생성시,수정 시  두 개 벡터 추가 및 토픽 추가 테스트
+# 다이어리 생성시,수정 시  두 개 벡터 추가 및 토픽 추가 테스트 해야함
 
 
-@app.route('/diary/create', methods=['POST'])
+@application.route('/diary/create', methods=['POST'])
 def vector():
     params = request.get_json()
     API.vector(params['diary_id'], params['title'], params['content'])
@@ -43,13 +55,12 @@ def vector():
 # white list
 
 
-# @app.before_request
+# @application.before_request
 # def limit_remote_addr():
 #     if request.remote_addr != '127.0.1.1':
 #         abort(403)  # Forbidden
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # host 등을 직접 지정하고 싶다면
+    application.run(debug=True)
     # app.run(host="127.0.0.1", port="5000", debug=True)
