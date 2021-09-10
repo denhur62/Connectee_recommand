93cf57d50881093350710045bfbd3ccedba3e21a
denhur62 <denhur62@naver.com>
2021-09-10 17:40:10 +0900

    clicks and comments data refine

--- test.py
+++ test.py
@@ -1,43 +1,34 @@
 import pandas as pd
-from database import db_execute
-
-# 감정 추가해야함
+from model.database import db_execute
 
 
 def get_click_comment():
-    sql = "select * from clicks"
-    cd = pd.DataFrame(db_execute(sql))
-    cd['createdAt'] = cd['createdAt'].dt.strftime('%y-%m-%d')
-    cd = cd.groupby(['userId', 'createdAt', 'diaryId'])[
-        'clickCount'].sum().reset_index()
-    cd = pd.pivot_table(cd, index=['userId', 'createdAt'],
-                        columns='diaryId', values='clickCount')
+    # clicks frame
+    sql = "select * from clicks where deletedAt is null"
+    re = db_execute(sql)
+    cf = pd.DataFrame(re)
+    cf['createdAt'] = cf['createdAt'].dt.strftime('%y-%m-%d')
+    cf['one'] = 1
+    cf = cf.groupby(['userId', 'createdAt', 'diaryId', 'emotionType', 'emotionLevel'])[
+        'one'].sum().reset_index()
+    cf_emotion = pd.pivot_table(cf, index=['userId', 'createdAt'],
+                                columns=['emotionType'], values='emotionLevel')
+    cf_diary = pd.pivot_table(cf, index=['userId', 'createdAt'],
+                              columns=['diaryId'], values='one')
+    cf = pd.merge(cf_emotion, cf_diary, 'left', on=['userId', 'createdAt'])
+    # comments frame
     sql = "select * from comments where deletedAt is null"
     result = db_execute(sql)
     dq = pd.DataFrame(result)
     dq['createdAt'] = dq['updatedAt'].dt.strftime('%y-%m-%d')
-    dq = dq.set_index(['userId', 'createdAt', 'diaryId'])
-    dq = dq[['emotionLevel']]
-    dq = pd.pivot_table(dq, index=['userId', 'createdAt'],
-                        columns='diaryId', values='emotionLevel')
-    return cd, dq
+    dq_emotion = pd.pivot_table(dq, index=['userId', 'createdAt'],
+                                columns=['userEmotionType'], values='userEmotionLevel')
+    dq_diary = pd.pivot_table(dq, index=['userId', 'createdAt'],
+                              columns=['diaryId'], values='emotionLevel')
+    dq = pd.merge(dq_emotion, dq_diary, 'left', on=['userId', 'createdAt'])
+    da = pd.concat([cf, dq]).fillna(0).sort_index()
+    print(da.shape)
+    return da
 
 
-# df, dq = get_click_comment()
-# da = pd.merge(df, dq, on=['userId', 'createdAt'],
-#               how='outer', suffixes=['', '_new'], sort=True).fillna(0)
-# check = False
-# cnt = 0
-# for i in da.columns:
-#     if check:
-#         check = False
-#         da = da.drop(columns=[i], axis=1)
-#         continue
-#     if isinstance(i, str):
-#         check = True
-#         da[i] = da[i]+da[da.columns[cnt+1]]
-#     cnt += 1
-# print(da)
-sql = "select interest from diaries where id=%s"
-diary_topic = db_execute(sql, [2])[0]['interest']
-print(diary_topic)
+get_click_comment()
